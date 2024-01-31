4855eeaeec8bb68b497180cb6d739815850e7acf
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2024-01-31 15:15:26 -0300

    create

--- src/main/java/com/rodrigop13/controller/UserController.java
+++ src/main/java/com/rodrigop13/controller/UserController.java
@@ -2,11 +2,17 @@ package com.rodrigop13.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rodrigop13.entity.User;
 import com.rodrigop13.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RequiredArgsConstructor //injeta as dependencias no caso autowired de userservice
@@ -16,4 +22,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     
     private final UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){  //encapsula o objeto user e depois enviado para o cliente
+        User usuario = userService.salvar(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
--- src/main/java/com/rodrigop13/service/UserService.java
+++ src/main/java/com/rodrigop13/service/UserService.java
@@ -1,7 +1,9 @@
 package com.rodrigop13.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rodrigop13.entity.User;
 import com.rodrigop13.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +13,9 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     
     private final UserRepository userRepository;
+
+    @Transactional
+    public User salvar(User user) {
+        return userRepository.save(user);
+    }
 }
