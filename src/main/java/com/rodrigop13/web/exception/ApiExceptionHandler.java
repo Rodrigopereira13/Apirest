7e558e4e20e0de663da881c4108bfaa5481c5e4b
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2024-03-26 15:10:33 -0300

    tratamento de error

---
+++ src/main/java/com/rodrigop13/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.rodrigop13.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    
+    @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex,
+    HttpServletRequest request,
+    BindingResult result){
+        log.error("Api Error", ex);
+        return ResponseEntity
+            .status(HttpStatus.UNPROCESSABLE_ENTITY)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result));
+
+    }
+}
